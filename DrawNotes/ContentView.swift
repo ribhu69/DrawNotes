73b6c30bd8accba3d0fa27e01ceb211ac9b6045e
Arkaprava Ghosh <arkaprava.ag@zohocorp.com>
2025-04-24 10:28:54 +0530

    Initial Commit

---
+++ DrawNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrawNotes
+//
+//  Created by Arkaprava Ghosh on 24/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrawNotes/DrawNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawNotesApp.swift
+//  DrawNotes
+//
+//  Created by Arkaprava Ghosh on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
