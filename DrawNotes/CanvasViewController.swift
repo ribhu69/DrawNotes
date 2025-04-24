aa4f906e16e7240525520ca25bd10c4a1fd2621c
Arkaprava Ghosh <arkapravaghosh99@gmail.com>
2025-04-24 14:03:14 +0530

    Add apple pencil double tap support to toggle between pencil and eraser

--- DrawNotes/CanvasViewController.swift
+++ DrawNotes/CanvasViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 import SwiftUI
 import PencilKit
 
-class CanvasViewController: UIViewController, PKToolPickerObserver, PKCanvasViewDelegate, UIScrollViewDelegate {
+class CanvasViewController: UIViewController, PKToolPickerObserver, PKCanvasViewDelegate, UIScrollViewDelegate , UIPencilInteractionDelegate{
     var canvasView: PKCanvasView!
     let scrollView = UIScrollView()
     let toolPicker = PKToolPicker()
-
+    
+    private var previousInkTool: PKTool?
+    private var isUsingEraser = false
     var contentSize = CGSize(width: 2000, height: 2000)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -46,6 +48,7 @@ class CanvasViewController: UIViewController, PKToolPickerObserver, PKCanvasView
         scrollView.addSubview(canvasView)
         scrollView.contentSize = contentSize
 
+        previousInkTool = canvasView.tool
         // Tool Picker
         if let window = view.window ?? UIApplication.shared.connectedScenes
             .compactMap({ ($0 as? UIWindowScene)?.keyWindow }).first {
@@ -55,7 +58,35 @@ class CanvasViewController: UIViewController, PKToolPickerObserver, PKCanvasView
             toolPicker.addObserver(self)
             canvasView.becomeFirstResponder()
         }
+
+        // Set up pencil interaction (double tap)
+        let pencilInteraction = UIPencilInteraction()
+        pencilInteraction.delegate = self
+        view.addInteraction(pencilInteraction)
     }
+    
+    
+    func pencilInteraction(_ interaction: UIPencilInteraction, didReceiveTap tap: UIPencilInteraction.Tap) {
+        pencilInteractionDidTap(interaction)
+    }
+    
+    func pencilInteractionDidTap(_ interaction: UIPencilInteraction) {
+        let currentTool = canvasView.tool
+
+        if currentTool is PKEraserTool {
+            // Switch back to previous ink tool if available
+            if let previousInk = previousInkTool {
+                canvasView.tool = previousInk
+                isUsingEraser = false
+            }
+        } else if let inkTool = currentTool as? PKInkingTool {
+            // Save current ink tool and switch to eraser
+            previousInkTool = inkTool
+            canvasView.tool = PKEraserTool(.vector)
+            isUsingEraser = true
+        }
+    }
+
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -103,13 +134,10 @@ class CanvasViewController: UIViewController, PKToolPickerObserver, PKCanvasView
         canvasView.drawing = PKDrawing(strokes: newStrokes)
     }
 
-    
     func invertedColor(from color: UIColor, against background: UIColor) -> UIColor {
         var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
         color.getRed(&r, green: &g, blue: &b, alpha: &a)
 
         return UIColor(red: 1.0 - r, green: 1.0 - g, blue: 1.0 - b, alpha: a)
     }
-
 }
-
